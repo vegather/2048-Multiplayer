d4be213411eb1b6fdd2ac18c5c56873f8187e3d4
Vegard Solheim Theriault <vegard@moonwearables.com>
2015-03-11 16:57:02 +0100

    Makes the gameBrain a generic Updates scores

--- GameBrain.swift
+++ GameBrain.swift
@@ -13,10 +13,6 @@ import Foundation
 // After that, any tiles that should merge will be on top of each other.
 // For these tiles, mergeTilesAtCoordinate will be called.
 protocol GameBrainDelegate: class {
-//    func spawnTile(tile: Tile)
-//    func moveTileFromCoordinate(from: Coordinate, toCoordinate to: Coordinate)
-//    func mergeTilesAtCoordinate(coordinate: Coordinate)
-    
     func performActions<T: Evolvable>(actions: [MoveAction<T>])
     
     func userHasNewScore(newUserScore: Int)
@@ -26,44 +22,55 @@ protocol GameBrainDelegate: class {
     func opponentsTurn()
 }
 
-class GameBrain: GameBoardDelegate {
- 
-    private enum Turn {
-        case User
-        case Opponent
-    }
-    
+private enum Turn {
+    case User
+    case Opponent
+}
+
+class GameBrain<T: Evolvable>: GameBoardDelegate {
+
     private var userScore = 0
     private var opponentScore = 0
-    private var currentPlayer = Turn.User
+    private var currentPlayer: Turn = Turn.User
     
-    private var gameBoard: GameBoard<TileValue>?
+    private var gameBoard: GameBoard<T>?
     private weak var delegate: GameBrainDelegate?
     
     init(delegate: GameBrainDelegate, dimension: Int) {
         self.delegate = delegate
-        self.gameBoard = GameBoard<TileValue>(delegate: self, dimension: dimension)
+        self.gameBoard = GameBoard<T>(delegate: self, dimension: dimension)
         
     } 
     
-    func moveInDirection(direction: MoveDirection) {
-        
+    func startGame() {
+        self.gameBoard?.spawnNewGamePieceAtRandomPosition()
+        self.gameBoard?.spawnNewGamePieceAtRandomPosition()
     }
+    
+    func moveInDirection(direction: MoveDirection) {
+        self.gameBoard?.moveInDirection(direction)
+        self.gameBoard?.spawnNewGamePieceAtRandomPosition()
+    }
+    
+    
     
     
     // -------------------------------
     // MARK: Game Board Delegate Methods
     // -------------------------------
     
-//    func spawnedGamePiece<T: Evolvable>(#position: Coordinate, value: T) {
-//        
-//    }
-    
     func performedActions<T: Evolvable>(actions: [MoveAction<T>]) {
-        
+        self.delegate?.performActions(actions)
     }
     
     func updateScoreBy(scoreIncrement: Int) {
-        
+        switch self.currentPlayer {
+        case .User:
+            self.userScore += scoreIncrement
+            self.delegate?.userHasNewScore(self.userScore)
+        case .Opponent:
+            self.opponentScore += scoreIncrement
+            self.delegate?.opponentHasNewScore(self.opponentScore)
+        }
     }
 }
